e60ec9636b07f7c402d909a320b2c1c68c99f2df
Liang Zhao <lzhaoee@bu.edu>
2016-01-31 18:52:31 -0500

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,9 @@
+package wazeApp;
+
+public class Edge {
+
+	Node 	start;	/* initial vertex of this edge */
+	Node 	end;	/* terminal vertex of this edge */
+	double	len;		/* length */
+	String	name;
+}
